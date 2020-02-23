b96ed01f8165a776ee500737d717a367d450700e
Takatsuka-mark <takatsuka.mark@gmail.com>
2020-02-22 22:54:02 -0500

    Tired to implement Node.JS and failed

---
+++ temp/index.js
@@ -0,0 +1,16 @@
+// const express = require('express');
+// const app = express();
+//
+// app.use(express.static(__dirname + "/views"));  // implement our html
+// app.use(express.static(__dirname + "/public")); // our js, css, images
+//
+// const server = app.listen(5000);
+// app.get('/', function (req, res) {
+//     res.sendFile('index.html');
+// });
+//
+// const dotenv = require('dotenv');
+// dotenv.config();
+//
+// const apiai = require('apiai')(process.env.API_KEY);
+
