3512381e5f2e0b178f05f1064f034485272ca78a
Takatsuka-Mark <takatsuka.mark@gmail.com>
2025-12-06 01:37:35 -0800

    updating to vite ssg

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
+import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
-import router from './router'
+import { routes } from './router'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+// export const createApp = ViteSSG(App, { routes })
+export const createApp = ViteSSG(
+    App,
+    { routes },
+    ({ app, router, routes, isClient, initialState }) => {
+        // optional: install plugins, etc.
+    },
+)
